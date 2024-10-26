55ed06630b863b287232666fff3fef516f30c01e
ocean6954 <oceanan699@gmail.com>
2024-10-26 23:51:12 +0900

    tokenの表示削除

--- src/services/api.js
+++ src/services/api.js
@@ -18,8 +18,6 @@ const getDateRange = period => {
 
 export const fetchData = async (username, period) => {
   const {from, to} = getDateRange(period);
-  console.log('tokenは', REACT_APP_GITHUB_TOKEN);
-
   const query = `
         {
           user(login: "${username}") {
